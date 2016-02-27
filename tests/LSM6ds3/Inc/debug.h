bb99a8666e13e5e1eb01de6c6edd861c97c7bb22
spoyler <andrey.sidorovya@ya.ru>
2016-02-27 12:30:38 +0300

    add test for LSM6ds3 correct CS set/reset when reading by spi

---
+++ tests/LSM6ds3/Inc/debug.h
@@ -0,0 +1,13 @@
+// Header:		
+// File Name: debug.h
+// Author:		andrey sidorov
+// Date:			21.02.2016
+
+#ifndef __DEBUG_H
+#define __DEBUG_H
+
+#define DEBUG_PRINTF(...) printf(__VA_ARGS__)
+int InitDebugUart();
+
+
+#endif
